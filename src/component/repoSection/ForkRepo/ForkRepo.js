69e05c31361d5a745f2094acf8133e6717dfe6e2
RgnDunes <singh.divyansh1802@gmail.com>
2021-01-20 09:43:32 +0530

    Add : Fork Repo Section

--- src/component/repoSection/ForkRepo/ForkRepo.js
+++ src/component/repoSection/ForkRepo/ForkRepo.js
@@ -7,7 +7,6 @@ export default class ForkRepo extends Component{
       super(props);
       this.state = {
         repos : []
-        
       }
     }
 
@@ -24,19 +23,42 @@ export default class ForkRepo extends Component{
               this.setState({
                 repos : result
               });
-              console.log("FORKED REPO FETCH SUCCESSFULL");
+              console.log("FORK REPO FETCH SUCCESSFULL");
             },
             (error) => {
-              console.log("FORKED REPO FETCH FAILED : ",error);
+              console.log("FORK REPO FETCH FAILED : ",error);
             }
           )
       }
 
+      extractDate(date){
+        return date.substring(0,10);
+      }
+
+      extractDescription(description){
+        return description ? (<p className="forkRepoDescription">Description : <span className="forkRepoDescriptionContent"> {description} </span></p>) : "";
+      }
+
     render(){
         return(
-          <div>
-          {this.state.repos.map(home => <div key={home.id}>{home.name}</div>)}
-        </div>
+            <div>
+              <div className="forkRepo">Forked Repositories</div>
+              {this.state.repos.map(home => 
+                <p>
+                  {home.fork ? (
+                    <div key={home.id} className="row">
+                      <p className="repoName">
+                        <a href={home.html_url}>{home.name}</a>
+                      </p>
+                      <p className="createdAt">
+                        Created on <span className="publicRepoDate"> {this.extractDate(home.created_at)}</span> <span className="divider"> | </span> Size : <span className="publicRepoSize">{home.size} KB</span> <span className="divider"> | </span> Default Branch : <span className="publicRepoDefaultBranch"> {home.default_branch} </span>
+                      </p>
+                        {this.extractDescription(home.description)}
+                    </div>
+                  ) : ""}
+                </p>
+              )}
+            </div>
         );
     }
 }
